0d442b7e3bc9daa26f9df9f0fb1607a77b63c976
velvitoff <velvitoff@gmail.com>
2022-08-16 14:54:03 +0300

    remove console.log

--- src/utils/translation/translate.js
+++ src/utils/translation/translate.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const translateRequest = (sourceText, targetLanguage) => {
-    console.log(`Translating ${sourceText} to ${targetLanguage}`);
     return axios.get('https://translate.googleapis.com/translate_a/single', {
         params: {
             client: 'gtx',
