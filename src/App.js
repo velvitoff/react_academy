c040127ccb4592dfe752c82a562c68028b54b56b
velvitoff <velvitoff@gmail.com>
2022-08-22 20:50:02 +0300

    basic login logout

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ import { BrowserRouter } from "react-router-dom";
 import store from './store';
 import UserSettingsProvider from './components/common/userSettingsProvider';
 import GeneralAppLayout from './components/common/generalAppLayout';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 export default function App() {
 
+
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <UserSettingsProvider>
-          <GeneralAppLayout />
-        </UserSettingsProvider>
-      </BrowserRouter>
+      <GoogleOAuthProvider clientId="188827934663-74rg170epr01st83bvbd82t9nmcevvjf.apps.googleusercontent.com">
+        <BrowserRouter>
+          <UserSettingsProvider>
+            <GeneralAppLayout />
+          </UserSettingsProvider>
+        </BrowserRouter>
+      </GoogleOAuthProvider>
     </Provider>
   );
 }
