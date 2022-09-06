8a1729e692649a985224489376756eadb0beb934
velvitoff <velvitoff@gmail.com>
2022-09-06 14:40:19 +0300

    blogs view handle user with no login

--- src/pages/blogs/index.jsx
+++ src/pages/blogs/index.jsx
@@ -1,33 +1,44 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 import { blogsRequest } from '../../services/bloggerService';
+import { selectIsLoggedIn } from '../../store/slices/userSettingsSlice';
 import BlogDisplay from './blogDisplay';
 import MainGridWrapper from './mainGridWrapper';
 import SkeletonBlogDisplay from './skeletonBlogDisplay';
+import Localize from '../../components/common/localize';
 
 export default function Blogs() {
 
+    const isLoggedIn = useSelector(selectIsLoggedIn);
     const [items, setItems] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [isLoadingError, setIsLoadingError] = useState(false);
 
 
     useEffect(() => {
-        setIsLoading(true);
-        blogsRequest()
-            .then((response) => {
-                setItems(response.data.items);
-                setIsLoadingError(false);
-            })
-            .catch((err) => {
-                console.log(err);
-                setIsLoadingError(true);
-            })
-            .finally(() => {
-                setIsLoading(false);
-            })
-    }, [])
+        if (isLoggedIn) {
+            setIsLoading(true);
+            blogsRequest()
+                .then((response) => {
+                    setItems(response.data.items);
+                    setIsLoadingError(false);
+                })
+                .catch((err) => {
+                    console.log(err);
+                    setIsLoadingError(true);
+                })
+                .finally(() => {
+                    setIsLoading(false);
+                })
+        }
+    }, [isLoggedIn])
 
+    if(!isLoggedIn){
+        return(
+            <Localize input={"You have to log in to see your list of blogs"}/>
+        );
+    }
     if (isLoading) {
         return (
             <MainGridWrapper>
@@ -37,16 +48,16 @@ export default function Blogs() {
         );
     }
     if (isLoadingError) {
-        return (<p>Error loading...</p>);
+        return (<Localize input={"Error loading..."}/>);
     }
     if (items.length === 0) {
-        return (<p>No blogs</p>);
+        return (<Localize input={"No blogs"}/>);
     }
 
     return (
         <MainGridWrapper>
             {items.map((item) => (
-                <BlogDisplay blog={item} key={item.id}/>
+                <BlogDisplay blog={item} key={item.id} />
             ))}
         </MainGridWrapper>
     );
